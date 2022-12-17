784b8d806c5cb082f2824b127cdb7aeca5937dd6
TxcToxic <93470009+TxcToxic@users.noreply.github.com>
2022-12-17 11:03:54 +0100

    v1.0.0

---
+++ main.py
@@ -0,0 +1,33 @@
+import keyboard
+import time
+import os
+
+
+def press_key(key, timeout, log: bool = False):
+    key_presses = 0
+    print("starting in 3 seconds...")
+    time.sleep(3)
+    while True:
+        try:
+            keyboard.send(str(key), do_press=True, do_release=False)
+            time.sleep(0.1)
+            keyboard.send(str(key), do_press=False, do_release=True)
+            if log:
+                key_presses += 1
+                print(f" [+] Pressed {key} | {key_presses}")
+            if "." in timeout:
+                time.sleep(float(timeout))
+            else:
+                time.sleep(int(timeout))
+        except KeyboardInterrupt:
+            return
+
+
+if __name__ == "__main__":
+    os.system("title Auto-Key by -TOXIC-#1835")
+    while True:
+        os.system("cls")
+        ckey = input(" key > ")
+        ctimeout = input(" timeout > ")
+        clog = input(" log pressed (y/n) > ")
+        press_key(ckey, ctimeout, log=True if clog.casefold() in ('y', 'yes') else False)
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Developer:
+-TOXIC-#1835 | [Discord](http://discord.com/users/856594604812009502) | [Website](http://cft-devs.xyz/) | [Youtube](https://www.youtube.com/@-toxic-1835)
+# Introduction:
+It's a simple tool for games like FiveM, by using this tool you can "autofarm"
